9cd0c2e5b3595a2cc0d5a65e414a622310a25a08
azshen-umich <55771349+azshen-umich@users.noreply.github.com>
2020-03-06 12:22:24 -0500

    Update Pokemon.cpp

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -7,5 +7,58 @@
 #include <array>
 #include <vector>
 #include <sstream>
-#include "Pokemon.h"
+#include "moveset.cpp"
 
+class Pokemon
+{
+    public:
+    //all the types
+    static constexpr const char* const TYPE_NORMAL = "Normal";
+    static constexpr const char* const TYPE_FIGHT = "Fighting";
+    static constexpr const char* const TYPE_FLYING = "Flying";
+    static constexpr const char* const TYPE_POISON = "Poison";
+    static constexpr const char* const TYPE_GROUND = "Ground";
+    static constexpr const char* const TYPE_ROCK = "Rock";
+    static constexpr const char* const TYPE_BUG = "Bug";
+    static constexpr const char* const TYPE_GHOST = "Ghost";
+    static constexpr const char* const TYPE_STEEL = "Steel";
+    static constexpr const char* const TYPE_FIRE = "Fire";
+    static constexpr const char* const TYPE_WATER = "Water";
+    static constexpr const char* const TYPE_GRASS = "Grass";
+    static constexpr const char* const TYPE_ELECTR = "Electric";
+    static constexpr const char* const TYPE_PSYCHIC = "Psychic";
+    static constexpr const char* const TYPE_ICE = "Ice";
+    static constexpr const char* const TYPE_DRAGON = "Dragon";
+    static constexpr const char* const TYPE_DARK= "Dark";
+    static constexpr const char* const TYPE_FAIRY = "Fairy";
+
+    Pokemon();
+    
+    //if the pokemon doesn't have a nickname
+    Pokemon(const std::string &name, const std::string &item, const std::string &ability, const std::string &nature, 
+            Moveset moves)
+            {
+
+            }
+
+    //if pokemon has a nickname
+    Pokemon(const std::string &nickname,
+            const std::string &name, const std::string &item, const std::string &ability, const std::string &nature, 
+            Moveset moves)
+            {
+
+            }
+
+    std::string getName() const
+    {
+        return nameOfPokemon;
+    }
+    std::string getType() const
+    {
+        return typeOfPokemon;
+    }
+           
+    private:
+    std::string nameOfPokemon;
+    std::string typeOfPokemon;
+}; 
