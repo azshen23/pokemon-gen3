14d90c136501acbc8458d89eb686be92112710ce
azshen-umich <55771349+azshen-umich@users.noreply.github.com>
2020-03-06 15:56:17 -0500

    bugs

---
+++ Utilities.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cassert>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <sstream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+string convertToString(char *a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0l i < size; i ++)
+    {
+        s = s + a[i];
+    }
+    return s;
+}
